2cc116746cc1b28892cc7cd175f6501eac99c0a5
Lucas Climenhage <Lucas.Climenhage@rwdi.com>
2024-05-18 18:57:25 -0400

    Rename project to work history

--- components/home/Timeline.vue
+++ components/home/Timeline.vue
@@ -5,28 +5,26 @@
     </h3>
     <div class="flex w-full flex-col gap-4">
       <NuxtLink
-        v-for="project in projects?.filter((work) => work.featured)"
-        :key="project.name"
+        v-for="work in workHistory"
+        :key="work.name"
         role="link"
         class="flex cursor-pointer items-center gap-2 rounded-lg px-4 py-2 hover:bg-secondary hover:text-main"
-        :to="project.release === 'soon' ? '/' : project.link"
-        :aria-label="'go to ' + project.name + ' project website'"
-        :target="project.release === 'soon' ? '_self' : '_blank'"
+        :to="work.release === 'soon' ? '/' : work.link"
+        :aria-label="'go to ' + work.name + ' company website'"
+        :target="work.release === 'soon' ? '_self' : '_blank'"
       >
         <span class="whitespace-nowrap">
-          {{ project.name }}
+          {{ work.name }}
         </span>
         <div class="mx-2 h-[0.1px] w-full bg-muted" />
         <span class="whitespace-nowrap text-muted">
           {{
-            project.release === "soon"
-              ? $t("global.soon") + "..."
-              : project.release
+            work.release === "soon" ? $t("global.soon") + "..." : work.release
           }}
         </span>
       </NuxtLink>
     </div>
-    <div @click="useRouter().push('/projects')">
+    <div @click="useRouter().push('/resume')">
       <span class="font-testimonial text-white-shadow cursor-pointer text-sm">
         {{ $t("global.see_more") }}
       </span>
@@ -37,8 +35,8 @@
 <script setup lang="ts">
 const { locale } = useI18n();
 
-const { data: projects } = await useAsyncData(
-  "projects",
+const { data: workHistory } = await useAsyncData(
+  "workHistory",
   () =>
     queryContent("/projects").locale(locale.value).sort({ release: -1 }).find(),
   {
