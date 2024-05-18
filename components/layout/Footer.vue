96e72e3c20380c0ad594d2745ac8a7e1be8ad759
Lucas Climenhage <Lucas.Climenhage@rwdi.com>
2024-05-18 18:45:06 -0400

    Update footer with better sizing

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="mt-6 pb-20">
+  <div class="mt-6 pb-10">
     <Divider />
-    <footer class="mx-auto flex max-w-7xl flex-col items-center gap-4 px-4 py-6">
-      <Logo :size="8" />
+    <footer
+      class="mx-auto flex max-w-7xl flex-col items-center gap-4 px-4 pt-6"
+    >
+      <Logo :size="12" />
       <span class="text-center text-sm text-muted">
-        © {{ new Date().getFullYear() }}, {{ useAppConfig().footerName }} - {{ $t("global.all_rights_reserved") }}.
+        © {{ new Date().getFullYear() }}, {{ useAppConfig().footerName }} -
+        {{ $t("global.all_rights_reserved") }}.
       </span>
     </footer>
   </div>
