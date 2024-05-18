25e2b3c4329926740f5b8de333a74e5a4f190a90
Lucas Climenhage <Lucas.Climenhage@rwdi.com>
2024-05-18 17:14:40 -0400

    Remove signature component

--- components/about/Intro.vue
+++ components/about/Intro.vue
@@ -2,9 +2,7 @@
 
 <template>
   <div class="relative flex flex-col gap-3 sm:ml-4">
-    <h3 class="text-lg text-muted">
-      Intro
-    </h3>
+    <h3 class="text-lg text-muted">Intro</h3>
     <div class="flex flex-col gap-4 text-main">
       <p>
         {{ $t("about.intro.part1") }}
@@ -19,8 +17,6 @@
         {{ $t("about.intro.part4") }}
       </p>
     </div>
-    <AboutSignature class="absolute -bottom-1/3 right-0 hidden w-40 sm:block" />
-    <AboutSignature class="black absolute -right-2 bottom-[-6rem] w-32 sm:hidden" />
   </div>
 </template>
 
--- components/content/About.vue
+++ components/content/About.vue
@@ -1,27 +1,29 @@
 <script setup lang="ts">
-const { data: stack } = await useAsyncData('stack', () => queryContent('/stack').findOne())
+const { data: stack } = await useAsyncData("stack", () =>
+  queryContent("/stack").findOne()
+);
 </script>
 
 <template>
   <section class="mx-auto mt-4 flex max-w-4xl flex-col p-7 sm:mt-20">
-    <h1 class="font-testimonial text-white-shadow text-center text-4xl font-bold">
+    <h1
+      class="font-testimonial text-white-shadow text-center text-4xl font-bold"
+    >
       <ContentSlot :use="$slots.title" />
     </h1>
     <h2 class="text-center text-lg font-light italic text-muted">
       <ContentSlot :use="$slots.description" />
     </h2>
     <Divider class="mb-8 mt-2" />
-    <div class="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+    <div
+      class="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between"
+    >
       <AboutProfilePicture />
       <div class="relative flex flex-col gap-3 sm:ml-4">
-        <h3 class="text-lg text-muted">
-          Intro
-        </h3>
+        <h3 class="text-lg text-muted">Intro</h3>
         <div class="flex flex-col gap-4 text-main">
           <ContentSlot :use="$slots.intro" />
         </div>
-        <AboutSignature class="absolute -bottom-24 right-0 hidden w-40 sm:block" />
-        <AboutSignature class="black absolute -bottom-24 -right-2 w-32 sm:hidden" />
       </div>
     </div>
     <Divider class="my-8" />
@@ -37,11 +39,7 @@ const { data: stack } = await useAsyncData('stack', () => queryContent('/stack')
         </p>
       </div>
       <div class="flex flex-wrap gap-4">
-        <AboutStackItem
-          v-for="item in stack!.items"
-          :key="item.name"
-          :item
-        />
+        <AboutStackItem v-for="item in stack!.items" :key="item.name" :item />
       </div>
     </div>
   </section>
