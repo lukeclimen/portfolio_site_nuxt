ca0c01d991ec6a94a546ca1f9d6966328a410b15
Lucas Climenhage <Lucas.Climenhage@rwdi.com>
2024-05-19 14:23:15 -0400

    Update skill icon assets

--- components/about/StackItem.vue
+++ components/about/StackItem.vue
@@ -1,35 +1,26 @@
+<template>
+  <SpotlightCard white class="p-6">
+    <NuxtLink :to="item.link" target="_blank" :aria-label="item.name + ' link'">
+      <component
+        :is="item.logo"
+        class="size-8 text-main"
+        :font-controlled="false"
+        :alt="item.name + ' logo'"
+        :filled="true"
+        :aria-label="item.name + ' logo'"
+      />
+    </NuxtLink>
+  </SpotlightCard>
+</template>
+
 <script setup lang="ts">
-import type { PropType } from 'vue'
-import type { Stack } from '~/data/stack'
+import type { PropType } from "vue";
+import type { Stack } from "~/data/stack";
 
 defineProps({
   item: {
     type: Object as PropType<Stack>,
     required: true,
   },
-})
+});
 </script>
-
-<template>
-  <SpotlightCard
-    white
-    class="p-6"
-  >
-    <NuxtLink
-      :to="item.link"
-      target="_blank"
-      :aria-label="item.name + ' link'"
-      class="flex gap-2"
-    >
-      <component
-        :is="item.logo"
-        class="size-8 text-main"
-        :class="item.logo.includes('Color') ? '' : 'text-main'"
-        :font-controlled="false"
-        :alt="item.name + ' logo'"
-        :filled="item.logo.includes('Color')"
-        :aria-label="item.name + ' logo'"
-      />
-    </NuxtLink>
-  </SpotlightCard>
-</template>
