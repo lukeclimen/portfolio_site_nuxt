9969b4994374d9be3451bce7e2866c9cd231a04b
Lucas Climenhage <Lucas.Climenhage@rwdi.com>
2024-05-19 15:46:19 -0400

    Add hover-over feature to icons
    The hover feature includes a highlight of the icon at the top, with the
    number of years of experience that I have with a given technology.

--- components/content/About.vue
+++ components/content/About.vue
@@ -21,17 +21,35 @@
     </div>
     <Divider class="my-8" />
     <div class="flex flex-col gap-3">
-      <div class="mb-6 flex flex-col gap-1">
-        <h3 class="text-white-shadow font-testimonial text-3xl font-bold">
-          <ContentSlot :use="$slots.stack_title" />
-        </h3>
-        <ContentSlot
-          :use="$slots.stack_description"
-          class="text-sm text-muted"
-        />
+      <div class="flex flex-row justify-between">
+        <div class="mb-6 flex flex-col gap-1">
+          <h3 class="text-white-shadow font-testimonial text-3xl font-bold">
+            <ContentSlot :use="$slots.stack_title" />
+          </h3>
+          <ContentSlot
+            :use="$slots.stack_description"
+            class="text-sm text-muted"
+          />
+        </div>
+        <div class="mb-6 flex flex-col text-end gap-1">
+          <h3 class="text-white-shadow font-testimonial text-3xl font-bold">
+            {{ highlightIconName }}
+          </h3>
+          <p v-if="highlightIconExperience !== ''" class="text-sm text-muted">
+            {{ highlightIconExperience }} years of experience
+          </p>
+        </div>
       </div>
       <div class="flex flex-wrap gap-4 justify-center">
-        <AboutStackItem v-for="item in stack!.items" :key="item.name" :item />
+        <AboutStackItem
+          v-for="icon in stack!.icons"
+          :key="icon.name"
+          :icon
+          @mouseenter="
+            handleIconHover(icon.name, icon.yearStarted, icon.yearEnded)
+          "
+          @mouseleave="handleIconExit"
+        />
       </div>
     </div>
   </section>
@@ -41,4 +59,26 @@
 const { data: stack } = await useAsyncData("stack", () =>
   queryContent("/stack").findOne(),
 );
+
+const highlightIconName = ref("");
+const highlightIconExperience = ref("");
+
+const handleIconHover = (
+  iconName: string,
+  yearStarted: number,
+  yearEnded: number,
+) => {
+  if (iconName !== "") {
+    highlightIconName.value = iconName;
+    let endYear: number;
+    if (yearEnded) endYear = yearEnded;
+    else endYear = new Date().getFullYear();
+    highlightIconExperience.value = String(endYear - yearStarted + 1);
+  } else highlightIconName.value = "";
+};
+
+const handleIconExit = () => {
+  highlightIconName.value = "";
+  highlightIconExperience.value = "";
+};
 </script>
