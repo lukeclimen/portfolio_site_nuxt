47f05009afe587d17ab9b3a523867015bec075db
Lucas Climenhage <luke.climenhage@gmail.com>
2024-05-23 20:43:11 -0400

    Provide fallback option to remove TS warning

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -3,7 +3,7 @@ import VueGtag, { trackRouter } from "vue-gtag-next";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: process.env.NUXT_GOOGLE_ANALYTICS_TAG,
+      id: process.env.NUXT_GOOGLE_ANALYTICS_TAG || "",
     },
   });
   trackRouter(useRouter());
