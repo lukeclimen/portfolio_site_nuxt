52ca40bf4817bf12e98012a811e95bc12464ba46
Lucas Climenhage <luke.climenhage@gmail.com>
2024-05-23 20:30:10 -0400

    Add GA4 tag to site with vue-gtag

---
+++ plugins/vue-gtag.client.ts
@@ -0,0 +1,10 @@
+import VueGtag, { trackRouter } from "vue-gtag-next";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueGtag, {
+    property: {
+      id: process.env.NUXT_GOOGLE_ANALYTICS_TAG,
+    },
+  });
+  trackRouter(useRouter());
+});
