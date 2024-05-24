1c22bd8f6628565e02a0c49476ad7209009b4f5f
Lucas Climenhage <luke.climenhage@gmail.com>
2024-05-23 21:01:37 -0400

    Update site metadata

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,76 +1,68 @@
 <script setup lang="ts">
-import { Toaster } from 'vue-sonner'
+import { Toaster } from "vue-sonner";
 
-const runtimeConfig = useRuntimeConfig()
-const appConfig = useAppConfig()
-const { appName, email } = useAppConfig()
-const { t } = useI18n()
-const { locale } = useI18n()
+const runtimeConfig = useRuntimeConfig();
+const appConfig = useAppConfig();
+const { appName, email } = useAppConfig();
+const { t } = useI18n();
+const { locale } = useI18n();
 
 useHead({
   titleTemplate: `%s - ${appName}`,
-  script: [
-    {
-      'src': 'https://plausible.hrcd.fr/js/script.js',
-      'defer': true,
-      'data-domain': 'canvas.hrcd.fr',
-    },
-  ],
+  script: [],
   link: [
     {
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico',
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/favicon.ico",
     },
     {
-      rel: 'apple-touch-icon',
-      sizes: '180x180',
-      href: '/apple-touch-icon.png',
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
     },
     {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '32x32',
-      href: '/favicon-32x32.png',
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
     },
     {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '16x16',
-      href: '/favicon-16x16.png',
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png",
     },
     {
-      rel: 'manifest',
-      href: '/site.webmanifest',
+      rel: "manifest",
+      href: "/site.webmanifest",
     },
   ],
-})
+});
 
 useSeoMeta({
-  description: () => t('global.app_description'),
-  author: 'Hugo Richard',
-  ogType: 'website',
+  description: () => t("global.app_description"),
+  author: "Luke Climenhage",
+  ogType: "website",
   ogTitle: appConfig.appName,
-  ogDescription: () => t('global.app_description'),
+  ogDescription: () => t("global.app_description"),
   ogUrl: () => runtimeConfig.public.siteUrl,
   ogLocale: () => locale.value,
   twitterTitle: appConfig.appName,
-  twitterDescription: () => t('global.app_description'),
-  twitterCard: 'summary_large_image',
-  twitterSite: appConfig.twitterUsername,
-  twitterCreator: appConfig.twitterUsername,
-  twitterImage: 'https://hrcd.fr/social-preview.jpg',
-})
+  twitterDescription: () => t("global.app_description"),
+  twitterCard: "summary_large_image",
+  twitterImage: "",
+});
 
 defineShortcuts({
   meta_o: {
     usingInput: true,
     handler: () => {
-      copyToClipboard(email)
-      toast.success(t('global.email_copied'))
+      copyToClipboard(email);
+      toast.success(t("global.email_copied"));
     },
   },
-})
+});
 </script>
 
 <template>
@@ -92,7 +84,9 @@ defineShortcuts({
           </ContentQuery>
         </NuxtLayout>
         <Toaster close-button />
-        <DotPattern class="absolute inset-0 -z-10 size-full fill-white/5 [mask-image:radial-gradient(white,transparent_85%)]" />
+        <DotPattern
+          class="absolute inset-0 -z-10 size-full fill-white/5 [mask-image:radial-gradient(white,transparent_85%)]"
+        />
       </Body>
     </Html>
   </div>
